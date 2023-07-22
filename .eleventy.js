02809092cc62b90066b3c9f7e1d1b7d148f6a5ce
Tobias Ulrich <tobiasbulrich@gmail.com>
2023-07-22 20:48:43 -0300

    Add basic project structure

---
+++ .eleventy.js
@@ -0,0 +1,56 @@
+const path = require("path")
+const Nunjucks = require("nunjucks");
+
+const SRC_DIR = __dirname;
+const INPUT_DIR = `${SRC_DIR}/pages`;
+
+
+// console.log(`Running from ${SRC_DIR}`)
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addWatchTarget("css");
+  eleventyConfig.addWatchTarget("posts");
+
+  // add posts collection
+  eleventyConfig.addCollection("posts", function(collection) {
+    return collection.getFilteredByGlob("./posts/**/*.md");
+  });
+
+  // disables layout resolution to improve build performance
+  eleventyConfig.setLayoutResolution(false);
+
+  // adds filter for human dates
+  eleventyConfig.addFilter("html_date", dateObj => {
+    const date = new Date(dateObj);
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    const hour = String(date.getHours() + 1).padStart(2, '0');
+    const minute = String(date.getMinutes()).padStart(2, '0');
+  
+    return `${year}-${month}-${day} ${hour}:${minute}`; 
+  });
+
+  // adds filter for HTML tag <time datetime="..." /> 
+  eleventyConfig.addFilter("readable_date", dateObj => {
+    const date = new Date(dateObj);
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+  
+    return `${year}-${month}-${day}`; 
+  });
+
+
+  return {
+    dir: {
+      input: ".",
+      output: "dist",
+      includes: 'includes',
+      layouts: "layouts",
+      htmlTemplateEngine: "njk",
+      markdownTemplateEngine: "njk"
+    }
+  }
+} 
---
+++ posts/2023-07-22-initial-post.md
@@ -0,0 +1,7 @@
+---
+layout: post.njk
+title: Initial post
+description: Just a test
+date: 2023-07-22 20:40:00 -03:00
+---
+Hello world!
